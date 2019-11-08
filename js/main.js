e218bb9babbd72f95637078de04fe981b5adc8d8
Regina Campos <rmscampos@gmail.com>
2019-11-08 10:02:13 -0600

    counter functionality

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
 //state vars
-const count = 0;
-const input = 1;
+var count = 0;
 
 //cached elements
-let countEl = document.getElementById('count')
-let inputEl = document.querySelector('input');
+let numberShown = document.getElementById('number');
+let input = document.querySelector('input');
 let addEl = document.getElementById('add');
-let subEl = document.getElementById('sub');
+let subEl = document.getElementById('subtract');
 
 //event listeners
 
-document.getElementById('add').addEventListener('click', addInput);
-document.getElementById('subtract').addEventListener('click', subInput);
-document.querySelectorAll('button').addEventListener('mouseover', changeButtonColor);
+addEl.addEventListener('click', addInput);
+subEl.addEventListener('click', subInput);
 
 
 //functions
 
-//the count is not adding to the input
-//
-
-function addInput(evt) {
-    countEl.textContent = (count + parseInt(inputEl.value, 10));
-} 
-
+function addInput() {
+    count += parseInt(input.value);
+    render()
+};
     
-function subInput(evt) {
-    countEl.textContent = (count - parseInt(inputEl.value, 10));
-    countEl.style.color = countEl.value >= 0 ? 'black' : 'red';
+function subInput() {
+    count -= parseInt(input.value);
+    render()
 }
 
-function changeButtonColor(evt){
-    event.target.style.color = 'grey';
-    event.target.style.backgroundColor = 'white';
+function init() {
+    numberShown.textContent = count;
+    input.value = 1;
+}
+
+function render() {
+    numberShown.style.color = count < 0 ? 'red' : 'black';
+    numberShown.textContent = count;
 }
